e704d1409e285f3f23bf68e509debb73f91a9616
jasperobiacoro <109198362+jasperobiacoro@users.noreply.github.com>
2022-10-23 19:03:49 -0700

    Update customer.java

--- Customer.java
+++ Customer.java
@@ -1,44 +1,38 @@
-/* 
+/** 
  * The purpose of this skeleton code is to explain the attributes and methods of an customer class. 
  * langauage: java
  * @author Jasper Obiacoro
- * Date modified: Oct 22, 2022
+ * Date modified: Oct 23, 2022
  */
+
 
 public class Customer 
 {
 	
-	
+	//class attributes of customer
 	private int customerID;
 	private String customerName;
 	private String address;
 	private String phoneNum;
 	private String email;
-
 	
-	//Constructor method for Customer class
-	public Customer(int customerID, String customerName, String address, String phoneNum, String email, boolean status)
+	//Constructor method for Customer object
+	public Customer(int customerID, String customerName, String address, String phoneNum, String email)
 	{
 		this.customerID = customerID;
 		this.customerName = customerName;
 		this.address = address;
 		this.phoneNum = phoneNum;
 		this.email = email;
-	
 	}
 	
-	//setter methods for each class attribute
-	/**
-     * setter method for customerID
-     * 
-     * @param  customerID  
-     */
+	//Setter method for each class attribute
 	public void setCustomerID(int CustomerID)
 	{
 		
 	}
 	
-	public void setCustomerName(String CustomerID)
+	public void setCustomerName(String CustomerName)
 	{
 		
 	}
@@ -58,32 +52,31 @@ public class Customer
 		
 	}
 	
-	
-	//getter methods for each class attribute
+	//Getter methods for each class attribute
 	public int getCustomerID()
 	{
-		
+		return customerID;
 	}
 	
 	public String getCustomerName()
 	{
-		
+		return customerName;
 	}
 	
 	public String getAddress()
 	{
-		
+		return address;
 	}
 	
 	public String getPhoneNum()
 	{
-		
+		return phoneNum;
 	}
+	
 	
 	public String getEmail()
 	{
-		
+		return email;
 	}
-	
 	
 }
