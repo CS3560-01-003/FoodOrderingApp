11a4c8d5b43510d8a9c30a40bda13eaa42c2bb84
PBallesteros02 <pballesteros@cpp.edu>
2022-10-23 17:24:23 -0700

    Create OrderItems.java

---
+++ OrderItems.java
@@ -0,0 +1,57 @@
+/*
+ * The purpose of this skeleton code is to demonstrate the attributes of OrderItems.
+ * langauge: java
+ * @author Priscilla 
+ * Date modified: Oct 23, 22
+ * 
+ */
+
+import java.io.*;
+import java.util.*;
+
+
+public class OrderItems 
+{
+    //Attributes of orderItems
+    int itemID;
+    int itemQuantity;
+    String itemName;
+
+    // Constructor of orderItems
+    public OrderItems(int itemID, int itemQuantity, String itemName)
+    {
+        this.itemID = itemID;
+        this.itemQuantity = itemQuantity;
+        this.itemName = itemName;
+
+    }
+
+    public void setItemID(int itemID)
+    {
+
+    }
+    public void setItemQuantity(int itemQuantity)
+    {
+
+    }
+    public void setItemName(int itemName)
+    {
+
+    }
+
+    // getter method for each class attribute
+    public int getItemID()
+    {
+
+    }
+    public int getItemQuantity()
+    {
+
+    }
+    public String getItemName()
+    {
+
+    }
+
+    
+}
