dee19a046d99589fdd54179412b5e541b1c970e0
Gelila21 <79760415+Gelila21@users.noreply.github.com>
2022-11-20 15:21:56 -0800

    Update Login.java

--- LoginP/Login.java
+++ LoginP/Login.java
@@ -1,21 +1,20 @@
 package LoginP;
 
 import AdminP.*;
+
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import LoginP.DBConnection;
+import UserP.DashBoard;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 
-
-public class Login extends javax.swing.JFrame
+public class Login extends JFrame
 {
     private JPanel Main2;
     private JRadioButton buyerRadioButton;
@@ -27,124 +26,123 @@ public class Login extends javax.swing.JFrame
     private JButton registerButton;
 
     DBConnection conn;
-
-   // public class GLogin()
-   // {
-
-  //  }
-
+    static JFrame frame = new JFrame("Login");
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame("Login");
+
         frame.setContentPane(new Login().Main2);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
-public Login()
-{
-    //initComponents();
 
-        loginButton.addActionListener(new ActionListener()
+    public Login()
+    {
+        //initComponents();
+
+            loginButton.addActionListener(new ActionListener()
+            {
+
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    DBConnection conn = new DBConnection(); // connecting ot database
+                    System.out.println("connected"); //connection test*/
+
+                    //get the inputted information from the text fields
+                    String user_name = userNameTextField.getText();
+                    String password = passwordTextField.getText();
+                    boolean is_buyer = buyerRadioButton.isSelected();
+                    boolean is_seller = sellerRadioButton.isSelected();
+
+                    try
+                    {
+                        int accessType = 1;
+                        if(is_seller) { accessType = 2; }
+                        Connection dbconn = conn.connectDB();                                 //AND accesstype = ?
+                        String query = "SELECT * FROM users WHERE username = ?  AND password = ?  AND accesstype = ? ";
+                        PreparedStatement pst = dbconn.prepareStatement(query);
+                       // Statement pst = dbconn.createStatement(query);
+
+                   //  pst.setString(1, user_name);
+
+                        pst.setString(1, user_name);
+                        pst.setString(2, password);
+                     //  pst.setInt(3,accessType);
+                        pst.setInt(3, accessType);
+
+                        ResultSet rs = pst.executeQuery();
+
+                        if(rs.next()){
+                            dispose();
+                            frame.setVisible(false);
+                            if (accessType == 1)
+                            {
+
+                                // cross checking incoming date with the db users data - is admin in db
+                                // if matches login
+
+                                // else we ask the user to renter credentials
+
+                                DashBoard userDashboard = new DashBoard();
+                                // set it visible and kill the previous form/window
+                                userDashboard.setVisible(true);
+                                //success
+                                // Render the dashboard that belongs to user view
+                                // display data for the user
+
+                            }
+                            else {
+                                // cross checking incoming date with the db users data
+                                // if matches login
+                                // else we ask the user to renter credentials
+
+                                //success
+                                // Render the dashboard that belongs to user view
+                                // display data for the user
+                                Admin adminPanelInstance = new Admin();
+                                adminPanelInstance.setTitle("Dashboard");
+                                adminPanelInstance.setVisible(true);
+
+
+                                /*adminPanelInstance.dispose();
+                                adminPanelInstance.setVisible(false);*/
+                            }
+                            frame.dispose();
+                            frame.setVisible(false);
+
+                        }
+                        else {
+                            JOptionPane.showMessageDialog(null,"Invalid username and/ password","",
+                                JOptionPane.ERROR_MESSAGE );
+
+                        }
+                        rs.close();
+                        pst.close();
+                    }catch(SQLException sqlException){
+                        sqlException.printStackTrace();
+                    }
+
+                }
+
+            });
+        registerButton.addActionListener(new ActionListener()
         {
-
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                DBConnection conn = new DBConnection(); // connecting ot database
-                System.out.println("connected"); //connection test
+                Registration newRInstance = new Registration();
+                newRInstance.setLocationRelativeTo(null);
+                newRInstance.setTitle("Registration");
+                newRInstance.setVisible(true);
 
-                //get the inputted information from the text fields
-                String user_name = userNameTextField.getText();
-                String password = passwordTextField.getText();
-                boolean is_buyer = buyerRadioButton.isSelected();
-                boolean is_seller = sellerRadioButton.isSelected();
-
-                Connection dbconn = conn.connectDB();
-
-                int accessType = 1;
-                if (is_seller)
-                {
-                    accessType = 2;
-                }
-
-                try
-                {
-                    PreparedStatement st = dbconn.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ? AND accesstype = ?");
-
-                    st.setString(1, user_name);
-                    st.setString(2, password);
-                    st.setInt(3, accessType);
-
-                    ResultSet res = st.executeQuery();
-
-                    if (res.next())
-                    {
-                        // display dashboard or new page after login.
-                        dispose();
-                    //    this.setVisible(false);
-                       // Login.setVisible(false);
-
-                    //   if (accessType == 2)
-                   //    {
-                   //   AdminP.Admin adminPanelInstance = new AdminP.Admin();
-                     //    adminPanelInstance.setLocationRelativeTo(null);
-                      //   adminPanelInstance.setTitle("Dashboard");
-                      //     adminPanelInstance.setVisible(true);
-                     //   }
-
-                        /**     if(accessType == 1) {
-                         Cart ShoppingCartInstance = new Cart(user_name);
-
-                         Dashboard dashBoardInstance = new Dashboard(ShoppingCartInstance);
-                         dashBoardInstance.setLocationRelativeTo(null);
-                         dashBoardInstance.setTitle("Dashboard");
-                         dashBoardInstance.setVisible(true);
-                         } else {
-                         AdminPanelHomePage adminPanelInstance = new AdminPanelHomePage();
-                         adminPanelInstance.setLocationRelativeTo(null);
-                         adminPanelInstance.setTitle("Dashboard");
-                         adminPanelInstance.setVisible(true);
-                         } **/
-                    }// else
-                //    {
-                 //       JOptionPane.showMessageDialog(this, "Invalid username and/or password.", "", JOptionPane.ERROR_MESSAGE);
-                  //  }
-
-                    res.close();
-                    st.close();
-
-                }
-                catch (SQLException ex)
-                {
-                    Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-             //   if (user_name.isEmpty() ||
-                    //    password.isEmpty())
-             //   {
-                   // JOptionPane.showMessageDialog(this, "Username / Password should not be empty.", "", JOptionPane.ERROR_MESSAGE);
-            //    } else
-            //    {
-            //        try
-             //       {
-             //           uLogin(user_name, password, is_buyer, is_seller);
-             //       }
-            //        catch (SQLException ex)
-            //        {
-                   //     Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-            //        }
-           //     }
+                frame.dispose();
+                frame.setVisible(false);
             }
-
-       
-
-
         });
-
-
     }
 
-
 }
+
+
 
