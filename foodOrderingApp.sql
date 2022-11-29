41b23d0fe6de54cb94c3a15025737bb094539591
jasperobiacoro <109198362+jasperobiacoro@users.noreply.github.com>
2022-11-29 01:19:08 -0800

    Update foodOrderingApp.sql

--- foodOrderingApp.sql
+++ foodOrderingApp.sql
@@ -17,7 +17,7 @@ Create Table Payment(
     userID INT (6), 
     customerName VARCHAR (45),
     cardType VARCHAR (45) NOT NULL,
-    cardNumber INT NOT NULL,
+    cardNumber BIGINT NOT NULL,
     cardExp INT NOT NULL,
     cardSecure INT NOT NULL, 
     FOREIGN KEY (userID) REFERENCES User(userID),
@@ -30,7 +30,7 @@ Create Table customerOrder(
     orderDate DATETIME,
     orderTime DATETIME,
     numOfProducts INT NOT NULL,
-    cardNumber INT NOT NULL,
+    cardNumber BIGINT NOT NULL,
     orderType VARCHAR (45),
     orderStatus VARCHAR (45),
     FOREIGN KEY (cardNumber) REFERENCES Payment(cardNumber),
@@ -54,8 +54,21 @@ Create Table orderItems(
     PRIMARY KEY (itemID)
     );
     
-INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('001', 'Classic Cheeseburge', 'juicy', '11.99', ?, 'in stock');
-INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('002', 'Boneless Chicken Wings', 'saucy', '16.99', ?, 'in stock');
-INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('003', 'Pecan Pie Slice', 'sweet', '3.99', ?, 'out of stock');
-INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('004', 'Class Jenny’s Breakfast', 'yum', '11.99', ?, 'in stock');
+INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('001', 'Classic Cheeseburge', 'juicy', '11.99', 'F:\foodOrderingApp\burger.jpg', 'in stock');
+INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('002', 'Boneless Chicken Wings', 'saucy', '16.99', 'F:\foodOrderingApp\wings.jpg', 'in stock');
+INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('003', 'Pecan Pie Slice', 'sweet', '3.99', 'F:\foodOrderingApp\pie.jpg', 'out of stock');
+INSERT INTO `foodorderingapp`.`menuitems` (`itemID`, `itemName`, `itemDescription`, `itemPrice`, `itemImage`, `itemAvailability`) VALUES ('004', 'Class Jenny’s Breakfast', 'yum', '11.99', 'F:\foodOrderingApp\breakfastcustomerorder.jpg', 'in stock');
 
+INSERT INTO `foodorderingapp`.`user` (`userID`, `userName`, `userAddress`, `loginUser`, `userEmail`, `loginPassword`, `accessType`) VALUES ('1', 'Priscilla Ballesteros', '3801 W Temple Ave, Pomona, CA 91768', 'Priscilla1', 'priscilla@gmail.com', 'p123', '1');
+INSERT INTO `foodorderingapp`.`user` (`userID`, `userName`, `userAddress`, `loginUser`, `userEmail`, `loginPassword`, `accessType`) VALUES ('2', 'Anushka Venkatesh', '3802 W Temple Ave, Pomona, CA 91768', 'Anushka2', 'anushka@gmail.com', 'a123', '1');
+INSERT INTO `foodorderingapp`.`user` (`userID`, `userName`, `userAddress`, `loginUser`, `userEmail`, `loginPassword`, `accessType`) VALUES ('3', 'Gelila Chaka', '3803 W Temple Ave, Pomona, CA 91768', 'Gelila3', 'gelila@gmail.com', 'g123', '1');
+INSERT INTO `foodorderingapp`.`user` (`userID`, `userName`, `userAddress`, `loginUser`, `userEmail`, `loginPassword`, `accessType`) VALUES ('4', 'Melody Duong', '3804 W Temple Ave, Pomona, CA 91768', 'Melody4', 'melody@gmail.com', 'm123', '2');
+INSERT INTO `foodorderingapp`.`user` (`userID`, `userName`, `userAddress`, `loginUser`, `userEmail`, `loginPassword`, `accessType`) VALUES ('5', 'Jasper Obiacoro', '3805 W Temple Ave, Pomona, CA 91768', 'Jasper5', 'jasper@gmail.com', 'j123', '2');
+INSERT INTO `foodorderingapp`.`user` (`userID`, `userName`, `userAddress`, `loginUser`, `userEmail`, `loginPassword`, `accessType`) VALUES ('6', 'Sean Solomon', '3806 W Temple Ave, Pomona, CA 91768', 'Sean6', 'sean@gmail.com', 's123', '2');
+
+INSERT INTO `foodorderingapp`.`payment` (`userID`, `customerName`, `cardType`, `cardNumber`, `cardExp`, `cardSecure`) VALUES ('1', 'Priscilla Ballesteros', 'Debit', '1111111111111111', '0122', '123');
+INSERT INTO `foodorderingapp`.`payment` (`userID`, `customerName`, `cardType`, `cardNumber`, `cardExp`, `cardSecure`) VALUES ('2', 'Anushka Venkatesh', 'Debit', '2222222222222222', '0223', '456');
+INSERT INTO `foodorderingapp`.`payment` (`userID`, `customerName`, `cardType`, `cardNumber`, `cardExp`, `cardSecure`) VALUES ('3', 'Gelila Chaka', 'Credit', '3333333333333333', '0324', '789');
+INSERT INTO `foodorderingapp`.`payment` (`userID`, `customerName`, `cardType`, `cardNumber`, `cardExp`, `cardSecure`) VALUES ('4', 'Melody Duong', 'Debit', '4444444444444444', '0425', '987');
+INSERT INTO `foodorderingapp`.`payment` (`userID`, `customerName`, `cardType`, `cardNumber`, `cardExp`, `cardSecure`) VALUES ('5', 'Jasper Obiacoro', 'Credit', '5555555555555555', '0526', '654');
+INSERT INTO `foodorderingapp`.`payment` (`userID`, `customerName`, `cardType`, `cardNumber`, `cardExp`, `cardSecure`) VALUES ('6', 'Sean Solomon', 'Debit', '6666666666666666', '0627', '321');
